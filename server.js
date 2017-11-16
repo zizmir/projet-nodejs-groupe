b32ac5e771756e90f2b511259e452fc5a971b028
m'bark <mbark.bouznir@gmail.com>
2017-11-16 11:45:14 +0100

    base project

---
+++ server.js
@@ -0,0 +1,31 @@
+
+//const http = require('http');
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.argv[2] || 8080;
+
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname,'public'));
+
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/',(req, res)=>{
+
+
+	res.render('index');
+
+});
+app.get('*',(req, res) =>{
+
+	res.render('404');
+});
+
+
+app.listen(port, (err) =>{
+	if(err) throw err;
+
+	console.log(`server listen at port ${port}`);
+})
