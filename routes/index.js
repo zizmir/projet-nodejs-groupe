1b1bdcef0d47d30bb846dfda555470b24febf39a
m'bark <mbark.bouznir@gmail.com>
2017-11-17 15:39:31 +0100

    add user in bdd

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-
 const db = require('../database/init');
 
 /* GET home page. */
@@ -9,6 +8,7 @@ router.get('/', (req, res) => {
     res.render('index', { count: nb });
   });
 });
+
 router.get('/sign-in', (req, res) => {
   
     res.render('sign-in')
@@ -16,14 +16,42 @@ router.get('/sign-in', (req, res) => {
 
 
 router.post('/sign-in',(req,res)=>{
+	let userForm = {
+		username: req.body.username,
+		password: req.body.password
+	}
+	db.user.findOne({where: {username:{username:userForm.username}}}).then(user => {
+		if(!user)
+		{
+			res.redirect('/sign-in');
+		}else if (user.checkPassword(userForm.password)) {
+			res.redirect('/');
+		}else{
+			res.redirect('/sign-in');
+		}
 
-	console.log('yoh')
+	})
 	// res.render('/')
 });
-router.get('/sign-out', (req, res) => {
-  
-    res.render('sign-out')
-});
+router.get('/sign-up', (req, res) => {
 
+  
+    res.render('sign-up');
+});
+router.post('/sign-up', (req, res) => {
+
+	let user = {
+		username: req.body.username,
+		firstname: req.body.firstname,
+		lastname: req.body.lastname,
+		password: req.body.password,
+		email: req.body.email,
+
+	};
+	console.log(req.body)
+	db.user.create(user);
+  
+    res.render('sign-up')
+});
 
 module.exports = router;
